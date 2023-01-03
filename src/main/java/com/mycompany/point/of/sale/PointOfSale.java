11f2d4a7538c18445605ec95b3cdd200e657bcc8
Tancredi M.S. <tancredi620@gmail.com>
2023-01-03 13:51:33 -0300

    first commit

---
+++ src/main/java/com/mycompany/point/of/sale/PointOfSale.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.point.of.sale;
+
+/**
+ *
+ * @author stanc
+ */
+public class PointOfSale {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
